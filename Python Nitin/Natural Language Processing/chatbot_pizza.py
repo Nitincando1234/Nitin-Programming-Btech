0a123c8aba763617ab70c22489aefe142c1d2889
Nitincando1234 <nitinsha322@gmail.com>
2024-10-10 19:06:21 +0530

    Latest NLP

---
+++ Python Nitin/Natural Language Processing/chatbot_pizza.py
@@ -0,0 +1,55 @@
+import json
+import mysql
+import mysql.connector
+import spacy
+nlp = spacy.load("en_core_web_lg")
+doc = nlp("I want two Greek pizza.")
+orderdict = {}
+def wordto_int(numword):
+    num = 0
+    try:
+        num = int(numword)
+    except ValueError:
+        words = ["zero", "one", "two", "three", "four", "five", "six", "seven",
+     "eight","nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
+     "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
+        for i, word in enumerate(words):
+            if word == numword: num = i
+    return num
+for token in doc:
+    if token.dep_ == "dobj":
+        dobj = token
+        orderdict.update(product = dobj.lemma_)
+        for child in dobj.lefts:
+            if child.dep_ == "amod" or child.dep_ == "compound":
+                orderdict.update(ptype = child.text)
+            elif child.dep_ == "det":
+                orderdict.update(qty = 1)
+            elif child.dep_ == "nummod":
+                orderdict.update(qty = wordto_int(child.text))
+        break
+
+# using mysql to implement in db
+json_dict = json.dumps(orderdict)
+try:
+    connection = mysql.connector.connect(user = "root", password = "123456", host = "127.0.0.1", database = "mybot")
+    cursor = connection.cursor()
+    query = """
+            INSERT INTO orders(product, ptype, qty) INTO 
+            SELECT (product, ptype, qty) FROM 
+            JSON_TABLE(
+            %s,
+            "$" COLUMNS(
+            product VARCHAR(20) PATH "$.product" ERROR ON EMPTY,
+            ptype VARCHAR(25) PATH "$.ptype"ERROR ON EMPTY, 
+            qty INT PATH "$.qty" ERROR ON EMPTY
+            )
+            ) AS jt
+            """
+    cursor.execute(query, (json_dict))
+    connection.commit()
+except mysql.connector.Error as e:
+    print(e.errno, ": {}".format(e.msg), " occured !")
+finally:
+    cursor.close()
+    connection.close()
