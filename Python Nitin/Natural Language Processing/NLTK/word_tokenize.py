40e9ad682e8d8cce73064007ea22b95303611f3d
Nitincando1234 <nitinsha422@gmail.com>
2024-10-29 10:30:29 +0530

    New

---
+++ Python Nitin/Natural Language Processing/NLTK/word_tokenize.py
@@ -0,0 +1,6 @@
+import nltk
+# nltk.download("punkt")
+
+from nltk.tokenize import sent_tokenize,  word_tokenize
+print(sent_tokenize("Nitin is great man !"))
+print(word_tokenize("Nitin is great man !"))
