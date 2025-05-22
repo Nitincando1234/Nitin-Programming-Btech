325e2b4ccfd6b8beadc94c8c2ea18e0ade4b9b62
Nitin Sharma <144478626+Nitincando1234@users.noreply.github.com>
2025-05-22 05:37:07 +0000

    Computer vision related

--- Information Security/DDOS_detection_using_scapy.py
+++ Information Security/DDOS_detection_using_scapy.py
@@ -1,10 +1,10 @@
+import time
 from scapy.all import sniff
 from collections import Counter
 
 # Configuration
-MONITOR_DURATION = 60
-THRESHOLD_REQUEST = 100
-TARGET_IP = "10.0.2.217"
+MONITOR_DURATION = 10
+THRESHOLD_REQUEST = 5
 
 requests_counter = Counter()
 
@@ -14,13 +14,16 @@ def process_packet(packet):
         ip_dst = packet["IP"].dest
         tcp_dport = packet["TCP"].dport
 
-        if ip_dst == TARGET_IP and tcp_dport == 80:     # For the HTTP requests
+        if ip_dst == TARGET_IP and tcp_dport == 5500:     # For the HTTP requests
             requests_counter[ip_src] += 1
 
 def intrusion_detect():
     print("Analysing...")
-    sniff(prn = process_packet, timeout = MONITOR_DURATION) 
+    sniff(filter = "tcp and dst port 5500",prn = process_packet, timeout = MONITOR_DURATION, store = False) 
     for ip_src, count in requests_counter.items():
         if count > THRESHOLD_REQUEST: print(f"Possible DOS intrusion detected from -> {ip_src} with requests: {count}")
 
-if __name__ == "__main__": intrusion_detect()    
+if __name__ == "__main__": 
+    TARGET_IP = input("Enter the Machine ip address: ")
+    intrusion_detect()   
+    time.sleep(10) 
